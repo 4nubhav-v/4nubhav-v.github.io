d1706234958bc1fb27abd5b1e852c5fc66bee0e4
Anubhav <131888766+4nubhav-v@users.noreply.github.com>
2024-08-16 22:44:55 +0000

    Add files via upload

---
+++ api/getTrack.js
@@ -0,0 +1,15 @@
+// api/getTrack.js
+export default async function handler(req, res) {
+  const API_KEY = process.env.LASTFM_API_KEY; // Use environment variables
+  const USERNAME = 'lostglory_';
+  const API_URL = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${USERNAME}&api_key=${API_KEY}&format=json`;
+
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching data' });
+  }
+}
+
