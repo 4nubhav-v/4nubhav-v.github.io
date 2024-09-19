1a01ef6ead84a4c39f385b00e29cdbfbb2b31f8d
Anubhav <4nubhav@proton.me>
2024-09-19 10:14:48 +0530

    Added netlify

--- last.js
+++ last.js
@@ -1,4 +1,4 @@
-const API_URL = "https://4nubhav.netlify.app/api/getTrack";
+const API_URL = "https://4nubhav.netlify.app/.netlify/functions/getTrack";
 
 async function fetchCurrentTrack() {
   try {
