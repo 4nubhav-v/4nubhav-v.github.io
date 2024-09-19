7b6a0ac7a034843a602e3b32df8fde9a31867521
Anubhav <131888766+4nubhav-v@users.noreply.github.com>
2024-09-19 10:52:31 +0530

    Update getTrack.js

--- netlify-functions/getTrack.js
+++ netlify-functions/getTrack.js
@@ -1,5 +1,5 @@
 // api/getTrack.js
-export default async function handler(req, res) {
+export default async function handler(event, context) {
   const API_KEY = process.env.LASTFM_API_KEY; // Use environment variables
   const USERNAME = process.env.LASTFM_USERNAME;
   const API_URL = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${USERNAME}&api_key=${API_KEY}&format=json`;
@@ -7,9 +7,17 @@ export default async function handler(req, res) {
   try {
     const response = await fetch(API_URL);
     const data = await response.json();
-    res.status(200).json(data);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching data' });
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error fetching data' }),
+    };
   }
 }
+
 
