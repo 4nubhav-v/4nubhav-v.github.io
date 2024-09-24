62c87f051863522aaf1d2f22bd10f2115b9b369f
4nubhav-v <4nubhav@proton.me>
2024-09-24 08:23:38 +0530

    Info added

--- api/getTrack.js
+++ api/getTrack.js
@@ -1,6 +1,6 @@
 // api/getTrack.js
 export default async function handler(req, res) {
-  const API_KEY = process.env.LASTFM_API_KEY; // Use environment variables
+  const API_KEY = process.env.LASTFM_API_KEY;
   const USERNAME = process.env.LASTFM_USERNAME;
   const API_URL = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${USERNAME}&api_key=${API_KEY}&format=json`;
 
@@ -9,7 +9,6 @@ export default async function handler(req, res) {
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching data' });
+    res.status(500).json({ error: "Error fetching data" });
   }
 }
-
---
+++ info.js
@@ -0,0 +1,4 @@
+const email = process.env.EMAIL;
+const phone = process.env.PHONE;
+const st = document.querySelectorAll('li');
+console.log(st);
