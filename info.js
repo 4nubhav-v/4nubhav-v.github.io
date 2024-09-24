f335d6506adc41836e8b8c6a83379690f72e3e99
4nubhav-v <4nubhav@proton.me>
2024-09-24 08:59:03 +0530

    Added info to env

--- info.js
+++ info.js
@@ -1,4 +1,8 @@
-const email = process.env.EMAIL;
-const phone = process.env.PHONE;
-const st = document.querySelectorAll('li');
-console.log(st);
+const email = process.env.NEXT_PUBLIC_EMAIL;
+const phoneno = process.env.NEXT_PUBLIC_PHONENO;
+let st1 = document.getElementById("info1").innerText;
+let st2 = document.getElementById("info2").innerText;
+st1 = `${st1} ${email}`;
+st2 = `${st2} ${phoneno}`;
+document.getElementById("info1").innerText = st1;
+document.getElementById("info2").innerText = st2;
