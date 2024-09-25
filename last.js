050f844efbb08bbdfb4155b1ba2dbaff89e0c431
4nubhav-v <4nubhav@proton.me>
2024-09-25 17:46:51 +0530

    chage in last.js

--- last.js
+++ last.js
@@ -32,6 +32,20 @@ async function fetchCurrentTrack() {
   }
 }
 
+fetch(`https://4nubhav.vercel.app/api/info`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(`Phone: ${data.phoneNumber}, Email: ${data.email}`);
+    let st1 = document.getElementById("info1").innerText;
+    let st2 = document.getElementById("info2").innerText;
+    st1 = `${st1} ${data.email}`;
+    st2 = `${st2} ${data.phoneNumber}`;
+    document.getElementById("info1").innerText = st1;
+    document.getElementById("info2").innerText = st2;
+  })
+  .catch((error) => console.error("Error fetching data:", error));
+
+
 // Fetch current track information on page load
 fetchCurrentTrack();
 
